ee7191c0f18da12989c4170649e5b9db760f7d3c
anwjrdid <yangchaehyeon67@gmail.com>
2026-04-30 10:39:16 +0900

    과제2 코딩과 테스트 완료 • 목표 § 마우스 드래그를 이용한 그림 그리기 기능 구현 • 내용 § 직선, 사각형, 원 그리기 기능 구현

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace SimplePaint
 {
     public partial class Form1 : Form
     {
-        // 도형 종류
         enum ToolType { Line, Rectangle, Circle }
 
-        // 현재 상태
+        private Bitmap canvasBitmap;
+        private Graphics canvasGraphics;
+
+        private bool isDrawing = false;
+        private Point startPoint;
+        private Point endPoint;
+
         private ToolType currentTool = ToolType.Line;
         private Color currentColor = Color.Black;
         private int currentLineWidth = 2;
@@ -18,15 +24,102 @@ namespace SimplePaint
         {
             InitializeComponent();
 
+            // ===== 캔버스 초기화 =====
+            canvasBitmap = new Bitmap(picCanvas.Width, picCanvas.Height);
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+            canvasGraphics.Clear(Color.White);
+            picCanvas.Image = canvasBitmap;
+
             // ===== 이벤트 연결 =====
             cmbColor.SelectedIndexChanged += cmbColor_SelectedIndexChanged;
-            cmbColor.SelectedIndex = 0; // 기본값
+            cmbColor.SelectedIndex = 0;
 
+            trbLineWidth.Minimum = 1;
+            trbLineWidth.Maximum = 10;
+            trbLineWidth.Value = 2;
             trbLineWidth.ValueChanged += trbLineWidth_ValueChanged;
 
             btnLine.Click += btnLine_Click;
             btnRectangle.Click += btnRectangle_Click;
             btnCircle.Click += btnCircle_Click;
+
+            picCanvas.MouseDown += PicCanvas_MouseDown;
+            picCanvas.MouseMove += PicCanvas_MouseMove;
+            picCanvas.MouseUp += PicCanvas_MouseUp;
+            picCanvas.Paint += PicCanvas_Paint;
+        }
+
+        // ===== 마우스 이벤트 =====
+        private void PicCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            startPoint = e.Location;
+        }
+
+        private void PicCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDrawing) return;
+
+            endPoint = e.Location;
+            picCanvas.Invalidate(); // 미리보기 갱신
+        }
+
+        private void PicCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!isDrawing) return;
+
+            isDrawing = false;
+            endPoint = e.Location;
+
+            using (Pen pen = new Pen(currentColor, currentLineWidth))
+            {
+                DrawShape(canvasGraphics, pen, startPoint, endPoint);
+            }
+
+            picCanvas.Invalidate();
+        }
+
+        // ===== Paint (미리보기) =====
+        private void PicCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (!isDrawing) return;
+
+            using (Pen pen = new Pen(currentColor, currentLineWidth))
+            {
+                pen.DashStyle = DashStyle.Dash; // 점선
+                DrawShape(e.Graphics, pen, startPoint, endPoint);
+            }
+        }
+
+        // ===== 도형 그리기 =====
+        private void DrawShape(Graphics g, Pen pen, Point p1, Point p2)
+        {
+            Rectangle rect = GetRectangle(p1, p2);
+
+            switch (currentTool)
+            {
+                case ToolType.Line:
+                    g.DrawLine(pen, p1, p2);
+                    break;
+
+                case ToolType.Rectangle:
+                    g.DrawRectangle(pen, rect);
+                    break;
+
+                case ToolType.Circle:
+                    g.DrawEllipse(pen, rect);
+                    break;
+            }
+        }
+
+        private Rectangle GetRectangle(Point p1, Point p2)
+        {
+            return new Rectangle(
+                Math.Min(p1.X, p2.X),
+                Math.Min(p1.Y, p2.Y),
+                Math.Abs(p1.X - p2.X),
+                Math.Abs(p1.Y - p2.Y)
+            );
         }
 
         // ===== 도형 선택 =====
@@ -45,26 +138,16 @@ namespace SimplePaint
             currentTool = ToolType.Circle;
         }
 
-        // ===== 색상 선택 =====
+        // ===== 색상 =====
         private void cmbColor_SelectedIndexChanged(object sender, EventArgs e)
         {
             switch (cmbColor.SelectedIndex)
             {
-                case 0:
-                    currentColor = Color.Black;
-                    break;
-                case 1:
-                    currentColor = Color.Red;
-                    break;
-                case 2:
-                    currentColor = Color.Blue;
-                    break;
-                case 3:
-                    currentColor = Color.Green;
-                    break;
-                default:
-                    currentColor = Color.Black;
-                    break;
+                case 0: currentColor = Color.Black; break;
+                case 1: currentColor = Color.Red; break;
+                case 2: currentColor = Color.Blue; break;
+                case 3: currentColor = Color.Green; break;
+                default: currentColor = Color.Black; break;
             }
         }
 
